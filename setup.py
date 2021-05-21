d245b7c04e07e96d4a82f4369fb87a52645147e4
Tim dW <tim@finitestate.io>
2021-05-21 10:57:06 -0400

    Increment Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="text_adventure_framework",
-    version="0.1.1",
+    version="0.1.2",
     author="Caesurus",
     author_email="caesurus+textadventure@gmail.com",
     description="A simple text adventure building framework",
